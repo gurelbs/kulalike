3dd3f916edee2340c17524bd924e1c94b7ba2358
gurelbs <guri240@gmail.com>
2021-10-13 20:44:21 +0300

    updated

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+const prod = process.env.NODE_ENV === 'production'
+const PORT = process.env.PORT || 4000;
+
+if (prod) {
+	app.use(express.static('build'))
+	app.get('/*', (req, res) => {
+		let file = path.join('build', 'index.html')
+		res.sendFile(file, { root: __dirname })
+	})
+}
+app.use((req,res) => {
+	res.status(404).send('404');
+})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
