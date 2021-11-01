28ecdd2feaec9a727268812eef7c7bb710e65c97
gurelbs <guri240@gmail.com>
2021-11-01 04:32:06 +0200

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const mongooseConnect = require('./utils/mongooseConnect.js')
 const api = require('./api')
 const app = express()
