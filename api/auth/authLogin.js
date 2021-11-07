4ea742962aea3a34c0ee84028ab3a45f6b4c003d
gurelbs <guri240@gmail.com>
2021-11-07 18:35:37 +0200

    updated

--- api/auth/authLogin.js
+++ api/auth/authLogin.js
@@ -5,7 +5,6 @@ const findOrCreateUser = require('../../controllers/findOrCreateUser')
 
 router.post('/auth/login', checkIfAuthenticated, findOrCreateUser, (req, res) => {
 	try {
-		console.log(req.user)
 		res.status(200).json('success')
 	} catch (error) {
 		console.log(error)
